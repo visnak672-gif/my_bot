86e1d7f22c95bb8418002381a2e3d3ebc37a4fee
visnak672-gif <visnak672@gmail.com>
2026-09-08 17:24:12 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '50px', textAlign: 'center' }}>
+      <h1>Telegram Webhook Bot is Running</h1>
+      <p>Next.js v16 & Vercel Webhook Server</p>
+    </div>
+  );
+}
